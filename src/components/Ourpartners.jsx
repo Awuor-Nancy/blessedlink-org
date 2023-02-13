65025ddb6db3131211423a16bb5956c3ad5f2661
Awuor-Nancy <anyangnancy@gmail.com>
2023-02-13 18:51:32 +0300

    added footer and contact sections

--- src/components/Ourpartners.jsx
+++ src/components/Ourpartners.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import bob from './images/bob.jpg'
-import doctor from './images/doctor.jpg,'
-import babutalent from './images/babutalent.jpg,'
-import kidsfarm from './images/kidsfarm.jpg,'
+// import doctor from './images/doctor.jpg,'
+// import babutalent from './images/babutalent.jpg,'
+// import kidsfarm from './images/kidsfarm.jpg,'
 
 const Ourpartners = ()=>{
     return (
@@ -10,35 +10,35 @@ const Ourpartners = ()=>{
 <div className="gallery">
 <div className="gallery">
   <a target="_blank" href="img_5terre.jpg">
-    <img src={bob} alt="bob" width="600" height="400" />
+    {/* <img src={bob} alt="bob" width="600" height="400" /> */}
   </a>
   <div className="desc">Add a description of the image here</div>
 </div>
 
 <div className="gallery">
   <a target="_blank" href="img_forest.jpg">
-    <img src={doctor} alt="doctor" width="600" height="400" />
+    {/* <img src={doctor} alt="doctor" width="600" height="400" /> */}
   </a>
   <div className="desc">Add a description of the image here</div>
 </div>
 
 <div className="gallery">
   <a target="_blank" href="img_lights.jpg">
-    <img src={babutalent} alt="Northern Lights" width="600" height="400" />
+    {/* <img src={babutalent} alt="Northern Lights" width="600" height="400" /> */}
   </a>
   <div className="desc">Add a description of the image here</div>
 </div>
 
 <div className="gallery">
   <a target="_blank" href="img_mountains.jpg">
-    <img src={kidsfarm} alt="Mountains" width="600" height="400" />
+    {/* <img src={kidsfarm} alt="Mountains" width="600" height="400" /> */}
   </a>
   <div className="desc">Add a description of the image here</div>
 </div>
 
 <div className="gallery">
   <a target="_blank" href="img_5terre.jpg">
-    <img src={babutalent} alt="Cinque Terre" width="600" height="400" />
+    {/* <img src={babutalent} alt="Cinque Terre" width="600" height="400" /> */}
   </a>
   <div className="desc">Add a description of the image here</div>
 </div>
@@ -52,14 +52,14 @@ const Ourpartners = ()=>{
 
 <div className="gallery">
   <a target="_blank" href="img_lights.jpg">
-    <img src={babutalent} alt="Northern Lights" width="600" height="400" />
+    {/* <img src={babutalent} alt="Northern Lights" width="600" height="400" /> */}
   </a>
   <div className="desc">Add a description of the image here</div>
 </div>
 
 <div className="gallery">
   <a target="_blank" href="img_mountains.jpg">
-    <img src={kidsfarm} alt="Mountains" width="600" height="400" />
+    {/* <img src={kidsfarm} alt="Mountains" width="600" height="400" /> */}
   </a>
   <div className="desc">Add a description of the image here</div>
 </div>
